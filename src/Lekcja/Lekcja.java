f8c04e5b3f320f283a2adb82c13634d7bb0f856c
MaOsu0 <mariusz.osuch1@gmail.com>
2022-02-16 18:09:18 +0100

    16.02.2022

---
+++ src/Lekcja/Lekcja.java
@@ -0,0 +1,32 @@
+package Lekcja;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Lekcja {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[2];
+        boolean error = true;
+
+        do {
+            try {
+                System.out.println("Podaj 1 liczbę");
+                numbers[0] = sc.nextInt();
+                System.out.println("Podaj 2 liczbę");
+                numbers[1] = sc.nextInt();
+
+                System.out.println("Którą wartośc chcesz wyświetlić ? 1 czy 2");
+                System.out.println("Liczba " + numbers[sc.nextInt() - 1]);
+                error = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Nie podałeś liczby całkowitej");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Miało być 1 lub 2, zacznijmy od nowa");
+            } finally {
+                sc.nextLine();
+            }
+        } while (error);
+        sc.close();
+    }
+}
